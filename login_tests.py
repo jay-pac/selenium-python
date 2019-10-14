f2eecf2189ccd64cb19c14d1edd213cbb5624723
Jason Pacitti <jason.pacitti@yeti.com>
2019-10-14 10:38:03 -0500

    removed comments, and some actions around navigation to the login page.

--- login_tests.py
+++ login_tests.py
@@ -1,34 +1,37 @@
+# TODO:  Need to find unqiue locator for Homepage Splash page / Modal popup
+# TODO: Add Assertions
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import *
 import unittest
 import time
-
 
 class LoginTests(unittest.TestCase):
 
     def setUp(self):
-        auth_url = "xxxxx"
+        base_url = 'https://Storefront:Yeti2017@staging-na-yeti.demandware.net/s/Yeti_US/en_US/login'
         self.driver = webdriver.Chrome()
         self.driver.implicitly_wait(10)
-        self.driver.get(auth_url)
+        self.driver.get(base_url)
+        self.driver.maximize_window()
+        
         try:
-            # element is dynamic need to update locator
             splash = self.driver.find_element_by_xpath('//*[@id="bx-element-1025412-TYHGubV"]/button')
             splash.click()
         except:
             pass
 
     def tests_login(self):
-
-        # sign_in_link = self.driver.find_element_by_xpath('//a[@class="header-utility-link header-banner-login"]')
-        # sign_in_link.click()
         email = self.driver.find_element_by_xpath('//*[@id="dwfrm_login"]//*[@type="email"]')
-        email.send_keys("xxxxx")
+        email.send_keys("generalOne@user.com")
 
         pwd = self.driver.find_element_by_xpath('//*[@id="dwfrm_login"]//*[@type="password"]')
-        pwd.send_keys("xxxxx")
-        time.sleep(10)
-        login_btn = self.driver.find_element_by_name('dwfrm_login_login')
+        pwd.send_keys("Generalone19!")
+        login_btn = WebDriverWait(self.driver, 20).until(EC.element_to_be_clickable((By.NAME, 'dwfrm_login_login')))
         login_btn.click()
 
     def tearDown(self):
