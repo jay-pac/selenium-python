9dff0abc5def8f9ecd4b05bb6037d8334e14e689
Jason Pacitti <jason.pacitti@yeti.com>
2019-12-05 14:11:10 -0600

    adding new page class for CLP

---
+++ sfcc/pages/category_page.py
@@ -0,0 +1,26 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.common.exceptions import *
+import time
+
+class CategoryPage():
+
+    def __init__(self, driver):
+        self.driver = driver
+    
+    def clickProduct(self):
+        product_tile = self.driver.find_element(By.XPATH, '//div[@data-product-name="Rambler Colster"]//img[@class="product-tile-image"]')
+        product_title.click()
+    
+    def clickAddToCartBtn(self):
+        add_to_cart_btn = self.driver.find_element(By.XPATH, '//div[@data-product-name="Rambler Colster"]//button[@class="button button-primary add-to-cart"]')
+        add_to_cart_btn.click()
+
+    def clickCustomizeBtn(self):
+        customize_btn = self.driver.find_element(By.XPATH, '//div[@data-product-name="Rambler Colster"]//span[@class="customizer-button-text"]')
+        customzie_btn.click()
+    
+    def clpSwatches(self):
+        swatch_color = self.driver.find_element(By.XPATH, '//div[@data-product-name="Rambler Colster"]//a[@data-yti="brick-red"]')
