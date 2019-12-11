f78c1636cf9043233d2382575a0d56eccec8d401
Jason Pacitti <jpacitti@gmail.com>
2019-12-11 17:35:09 -0600

    adding back the chrome setting

--- sfcc/tests/checkout/clp_add_stock_checkout_tests.py
+++ sfcc/tests/checkout/clp_add_stock_checkout_tests.py
@@ -17,7 +17,7 @@ class ClpAddStockCheckout(unittest.TestCase):
         6. Place order to complete
         """
         base_url = 'https://Storefront:Yeti2017@staging-na-yeti.demandware.net/s/Yeti_US/en_US/drinkware'
-        driver = webdriver.Firefox()
+        driver = webdriver.Chrome()
         driver.implicitly_wait(10)
         driver.get(base_url)
         driver.maximize_window()
