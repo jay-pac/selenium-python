b140bdcf9ca2e113f7bb92685d3d093c2b8a243e
Jason Pacitti <jpacitti@gmail.com>
2019-12-10 20:59:15 -0600

    fixed some formatting issues

--- sfcc/pages/category_page.py
+++ sfcc/pages/category_page.py
@@ -12,17 +12,21 @@ class CategoryPage():
         self.driver = driver
     
     def clickProduct(self):
-        product_tile = self.driver.find_element(By.XPATH, '//div[@data-product-name="Rambler Colster"]//img[@class="product-tile-image"]')
+        product_tile = self.driver.find_element(By.XPATH,
+                                                '//div[@data-product-name="Rambler Colster"]//img[@class="product-tile-image"]')
         product_tile.click()
     
     def clickAddToCartBtn(self):
-        add_to_cart_btn = self.driver.find_element(By.XPATH, '//div[@data-product-name="Rambler Colster"]//button[@class="button button-primary add-to-cart"]')
+        add_to_cart_btn = self.driver.find_element(By.XPATH,
+                                                   '//div[@data-product-name="Rambler Colster"]//button[@class="button button-primary add-to-cart"]')
         add_to_cart_btn.click()
 
     def clickCustomizeBtn(self):
-        customize_btn = self.driver.find_element(By.XPATH, '//div[@data-product-name="Rambler Colster"]//span[@class="customizer-button-text"]')
+        customize_btn = self.driver.find_element(By.XPATH,
+                                                 '//div[@data-product-name="Rambler Colster"]//span[@class="customizer-button-text"]')
         customize_btn.click()
     
     def clpSwatches(self):
-        swatch_color = self.driver.find_element(By.XPATH, '//div[@data-product-name="Rambler Colster"]//a[@data-yti="brick-red"]')
+        swatch_color = self.driver.find_element(By.XPATH,
+                                                '//div[@data-product-name="Rambler Colster"]//a[@data-yti="brick-red"]')
         swatch_color.click()
