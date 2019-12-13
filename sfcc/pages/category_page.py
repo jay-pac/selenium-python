b0c24f15fcbd10cef153c9dcc279032142269104
Jason Pacitti <jpacitti@gmail.com>
2019-12-13 15:27:19 -0600

    created section for locators

--- sfcc/pages/category_page.py
+++ sfcc/pages/category_page.py
@@ -10,25 +10,28 @@ class CategoryPage():
 
     def __init__(self, driver):
         self.driver = driver
+
+    # Locators
+    _product_tile = '//div[@data-product-name="Rambler Colster"]//img[@class="product-tile-image"]'
+    _add_to_cart = '//div[@data-product-name="Rambler Colster"]//button[@class="button button-primary add-to-cart"]'
+    _customize = '//div[@data-product-name="Rambler Colster"]//span[@class="customizer-button-text"]'
+    _swatch = '//div[@data-product-name="Rambler Colster"]//a[@data-yti="brick-red"]'
+
     
     def clickProduct(self):
-        product_tile = self.driver.find_element(By.XPATH,
-                                                '//div[@data-product-name="Rambler Colster"]//img[@class="product-tile-image"]')
+        product_tile = self.driver.find_element(By.XPATH, self._product_tile)
         product_tile.click()
     
     def clickAddToCartBtn(self):
         time.sleep(5)
-        add_to_cart_btn = self.driver.find_element(By.XPATH,
-                                                   '//div[@data-product-name="Rambler Colster"]//button[@class="button button-primary add-to-cart"]')
+        add_to_cart_btn = self.driver.find_element(By.XPATH, self._add_to_cart)
         add_to_cart_btn.click()
 
     def clickCustomizeBtn(self):
-        customize_btn = self.driver.find_element(By.XPATH,
-                                                 '//div[@data-product-name="Rambler Colster"]//span[@class="customizer-button-text"]')
+        customize_btn = self.driver.find_element(By.XPATH, self._customize)
         customize_btn.click()
     
     def clpSwatches(self):
         time.sleep(5)
-        swatch_color = self.driver.find_element(By.XPATH,
-                                                '//div[@data-product-name="Rambler Colster"]//a[@data-yti="brick-red"]')
+        swatch_color = self.driver.find_element(By.XPATH, self._swatch)
         swatch_color.click()
