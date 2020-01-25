820998cee12af04ba84f58cb32aaf6fceb0a797c
Jason Pacitti <jpacitti@gmail.com>
2020-01-24 18:33:39 -0600

    new page class for registered accounts

---
+++ sfcc/pages/account_page.py
@@ -0,0 +1,50 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.common.exceptions import *
+import time
+
+class AccountPage():
+
+    def __init__(self, driver):
+        self.driver = driver
+
+    # Locators
+    _customer_fn = 'dwfrm_profile_customer_firstname'
+    _customer_ln = 'dwfrm_profile_customer_lastname'
+    _customer_email = 'dwfrm_profile_customer_email'
+    _confirm_email = 'dwfrm_profile_customer_emailconfirm'
+    _customer_psw = '//form[contains(@id,"RegistrationForm")]//input[contains(@id,"profile_login_password_")]'
+    _confirm_psw = '//form[contains(@id,"RegistrationForm")]//input[contains(@id,"profile_login_passwordconfirm_")]'
+    _form_sign_in = 'dwfrm_profile_confirm'
+
+    def enterFirstName(self):
+        fn = self.driver.find_element(By.ID, self._customer_fn)
+        fn.send_keys('Fred')
+
+    def enterLastName(self):
+        ln = self.driver.find_element(By.ID, self._customer_ln)
+        ln.send_keys('Flintstone')
+
+    def enterEmail(self):
+        email = self.driver.find_element(By.ID, self._customer_email)
+        email.send_keys('yabba_dabba@yeti.com')
+
+    def confirmEmail(self):
+        email = self.driver.find_element(By.ID, self._confirm_email)
+        email.send_keys('yabba_dabba@yeti.com')
+
+    def enterPassword(self):
+        psw = self.driver.find_element(By.XPATH, self._customer_psw)
+        psw.send_keys('Tester123')
+
+    def confirmPassword(self):
+        psw = self.driver.find_element(By.XPATH, self._confirm_psw)
+        psw.send_keys('Tester123')
+
+    def clickSignUpBtn(self):
+        signup_btn = self.driver.find_element(By.NAME, self._form_sign_in)
+        signup_btn.click()
+
+
---
+++ sfcc/tests/account/account_creation_tests.py
@@ -0,0 +1,35 @@
+from selenium import webdriver
+from sfcc.pages.account_page import AccountPage
+import unittest
+
+
+class AccountCreationTests(unittest.TestCase):
+
+    def tests_create_account(self):
+        base_url = 'https://Storefront:Yeti2017@staging-na-yeti.demandware.net/s/Yeti_US/en_US/login'
+        driver = webdriver.Chrome()
+        driver.implicitly_wait(10)
+        driver.get(base_url)
+
+        cookie = {
+            'domain': 'staging-na-yeti.demandware.net',
+            'httpOnly': False,
+            'name': 'consent-accepted',
+            'path': '/',
+            'secure': False,
+            'value': 'true'}
+        driver.add_cookie(cookie)
+        driver.refresh()
+
+        driver.maximize_window()
+
+        account = AccountPage(driver)
+
+        account.enterFirstName()
+        account.enterLastName()
+        account.enterEmail()
+        account.confirmEmail()
+        account.enterPassword()
+        account.confirmPassword()
+        account.clickSignUpBtn()
+
