c117e22d3ce34cb53c96884664ee34dcaad6963c
Jason Pacitti <jpacitti@gmail.com>
2020-02-04 19:22:58 -0600

    removed locators and methods

--- sfcc/pages/account/account_page.py
+++ sfcc/pages/account/account_page.py
@@ -14,14 +14,6 @@ class AccountPage():
         self.driver = driver
 
     # Locators
-    _customer_fn = 'dwfrm_profile_customer_firstname'
-    _customer_ln = 'dwfrm_profile_customer_lastname'
-    _customer_email = 'dwfrm_profile_customer_email'
-    _confirm_email = 'dwfrm_profile_customer_emailconfirm'
-    _customer_psw = '//form[contains(@id,"RegistrationForm")]//input[contains(@id,"profile_login_password_")]'
-    _confirm_psw = '//form[contains(@id,"RegistrationForm")]//input[contains(@id,"profile_login_passwordconfirm_")]'
-    _form_sign_in = 'dwfrm_profile_confirm'
-
     _manage_address = '//a[contains(text(),"manage addresses")]'
     _address_button = '//a[@class="button-primary section-header-note address-create"]'
     _address_nickname = 'dwfrm_profile_address_addressid'
@@ -45,37 +37,6 @@ class AccountPage():
     _cc_year = 'c-exyr'
     _cc_cvv = 'lbl-c-cvv'
     _cc_apply = 'applyBtn'
-
-    def enterFirstName(self):
-        fn = self.driver.find_element(By.ID, self._customer_fn)
-        fn.send_keys('Fred')
-
-    def enterLastName(self):
-        ln = self.driver.find_element(By.ID, self._customer_ln)
-        ln.send_keys('Flintstone')
-
-    def enterEmail(self):
-        # running into error when attempting to store the email value in the confirm email method.
-        timestamp = datetime.now().strftime('%y%m%d%H%M')
-        email = self.driver.find_element(By.ID, self._customer_email)
-        email.send_keys(f'qa{timestamp}@yeti.com')
-
-    def confirmEmail(self):
-        timestamp = datetime.now().strftime('%y%m%d%H%M')
-        email = self.driver.find_element(By.ID, self._confirm_email)
-        email.send_keys(f'qa{timestamp}@yeti.com')
-
-    def enterPassword(self):
-        psw = self.driver.find_element(By.XPATH, self._customer_psw)
-        psw.send_keys('Tester123')
-
-    def confirmPassword(self):
-        psw = self.driver.find_element(By.XPATH, self._confirm_psw)
-        psw.send_keys('Tester123')
-
-    def clickSignUpBtn(self):
-        signup_btn = self.driver.find_element(By.NAME, self._form_sign_in)
-        signup_btn.click()
 
     # Add Address methods
     def clickAddressLink(self):
