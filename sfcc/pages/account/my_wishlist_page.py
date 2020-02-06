1c97948eea0e4faedacd71b06da35495ef6360f9
Jason Pacitti <jpacitti@gmail.com>
2020-02-06 15:52:49 -0600

    adding new pages and tests for the wishlist

--- sfcc/pages/account/my_wishlist_page.py
+++ sfcc/pages/account/my_wishlist_page.py
@@ -1,6 +1,24 @@
+from selenium.webdriver.common.by import By
+
+
 class MyWishListPage():
 
     def __init__(self, driver):
         self.driver = driver
 
     # Locators
+    _add_cart = 'dwfrm_wishlist_items_i0_addToCart'
+    _edit_link = 'dwfrm_wishlist_items_i0_updateItem'
+    _remove_link = 'dwfrm_wishlist_items_i0_deleteItem'
+
+    def clickAddToCart(self):
+        add_cart_btn = self.driver.find_element(By.NAME, self._add_cart)
+        add_cart_btn.click()
+
+    def clickEditLink(self):
+        edit_link = self.driver.find_element(By.NAME, self._edit_link)
+        edit_link.click()
+
+    def clickRemoveLink(self):
+        remove_link = self.driver.find_element(By.NAME, self._remove_link)
+        remove_link.click()
---
+++ sfcc/tests/account/add_wishlist_cart.py
@@ -0,0 +1,50 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from sfcc.pages.product_page import ProductPage
+from sfcc.pages.login.login_page import LoginPage
+from sfcc.pages.checkout_page import CheckoutPage
+from sfcc.pages.account.my_wishlist_page import MyWishListPage
+from sfcc.pages.account.account_page import AccountPage
+import unittest
+
+
+class AddWishListCartTests(unittest.TestCase):
+
+    def setUp(self):
+        base_url = 'https://Storefront:Yeti2017@staging-na-yeti.demandware.net/s/Yeti_US/en_US/login'
+        self.driver = webdriver.Chrome()
+        self.driver.implicitly_wait(10)
+        self.driver.get(base_url)
+
+        cookie = {
+            'domain': 'staging-na-yeti.demandware.net',
+            'httpOnly': False,
+            'name': 'consent-accepted',
+            'path': '/',
+            'secure': False,
+            'value': 'true'}
+
+        self.driver.add_cookie(cookie)
+        self.driver.refresh()
+
+        self.driver.maximize_window()
+
+        self.lp = LoginPage(self.driver)
+        self.pdp = ProductPage(self.driver)
+        self.checkout = CheckoutPage(self.driver)
+        self.mwlp = MyWishListPage(self.driver)
+        self.ap = AccountPage(self.driver)
+
+        self.lp.login('yabba_dabba@yeti.com', 'Tester123')
+
+    def tests_add_wishlist_cart(self):
+        self.ap.clickMyWishListLink()
+        self.mwlp.clickAddToCart()
+
+        self.pdp.clickMiniCart()
+
+        self.checkout.shippingBtn()
+        self.checkout.accountPayment('111')
+
+    def tearDown(self):
+        self.driver.quit()
