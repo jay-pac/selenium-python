9c133b847156cfdde2da88386bd3a36f67e0c1a9
Jason Pacitti <jpacitti@gmail.com>
2020-02-26 22:13:09 -0600

    updated all customizer tests to start using pytest, conftest, and removed all env variables and added to the conftest

--- sfcc/tests/customizer/custom_design_order_tests.py
+++ sfcc/tests/customizer/custom_design_order_tests.py
@@ -4,56 +4,29 @@ from sfcc.pages.login.login_page import LoginPage
 from sfcc.pages.customize_page import CustomizePage
 from sfcc.pages.product_page import ProductPage
 from sfcc.pages.checkout_page import CheckoutPage
-import unittest
+import pytest
 
 
-class CustomDesignOrderTests(unittest.TestCase):
-
-    def setUp(self):
-        base_url = 'https://Storefront:Yeti2017@staging-na-yeti.demandware.net/s/Yeti_US/en_US/login'
-        self.driver = webdriver.Chrome()
-        self.driver.implicitly_wait(10)
-        self.driver.get(base_url)
-
-        cookie = {
-            'domain': 'staging-na-yeti.demandware.net',
-            'httpOnly': False,
-            'name': 'consent-accepted',
-            'path': '/',
-            'secure': False,
-            'value': 'true'}
-
-        self.driver.add_cookie(cookie)
-        self.driver.refresh()
-
-        self.driver.maximize_window()
-
-        self.lp = LoginPage(self.driver)
-        self.pdp = ProductPage(self.driver)
-        self.checkout = CheckoutPage(self.driver)
-        self.custom = CustomizePage(self.driver)
-
-        self.lp.login('jason.pacitti@yeti.com', 'tester123')
+@pytest.mark.usefixtures("setup")
+class TestCustomDesignOrder():
 
     def tests_custom_design_order(self):
-        product_url = 'https://Storefront:Yeti2017@staging-na-yeti.demandware.net/s/Yeti_US/en_US/drinkware/rambler-20-oz-tumbler/YRAM20.html'
-        self.driver.get(product_url)
+        pdp = ProductPage(self.driver)
+        checkout = CheckoutPage(self.driver)
+        custom = CustomizePage(self.driver)
 
-        self.pdp.pdpSwatches('Black')
+        pdp.pdpSwatches('Black')
 
-        self.custom.pdpClickCustomButton()
-        self.custom.customModal()
-        self.custom.selectCustomDesign()
-        self.custom.clickApproval()
-        self.custom.clickAddToCart()
+        custom.pdpClickCustomButton()
+        custom.customModal()
+        custom.selectCustomDesign()
+        custom.clickApproval()
+        custom.clickAddToCart()
 
-        self.pdp.clickMiniCart()
+        pdp.clickMiniCart()
 
-        self.checkout.shippingBtn()
-        self.checkout.accountPayment('111')
+        checkout.shippingBtn()
+        checkout.accountPayment('111')
 
         order_number = self.driver.find_element(By.XPATH, '//p[@class="order-number"]//a').text
         print(order_number)
-
-    def tearDown(self):
-        self.driver.quit()
--- sfcc/tests/customizer/custom_logo_order_tests.py
+++ sfcc/tests/customizer/custom_logo_order_tests.py
@@ -1,59 +1,29 @@
-from selenium import webdriver
 from selenium.webdriver.common.by import By
-from sfcc.pages.login.login_page import LoginPage
 from sfcc.pages.customize_page import CustomizePage
 from sfcc.pages.product_page import ProductPage
 from sfcc.pages.checkout_page import CheckoutPage
-import unittest
+import pytest
 
 
-class CustomLogoOrderTests(unittest.TestCase):
-
-    def setUp(self):
-        base_url = 'https://Storefront:Yeti2017@staging-na-yeti.demandware.net/s/Yeti_US/en_US/login'
-        self.driver = webdriver.Chrome()
-        self.driver.implicitly_wait(10)
-        self.driver.get(base_url)
-
-        cookie = {
-            'domain': 'staging-na-yeti.demandware.net',
-            'httpOnly': False,
-            'name': 'consent-accepted',
-            'path': '/',
-            'secure': False,
-            'value': 'true'}
-
-        self.driver.add_cookie(cookie)
-        self.driver.refresh()
-
-        self.driver.maximize_window()
-
-        self.lp = LoginPage(self.driver)
-        self.pdp = ProductPage(self.driver)
-        self.checkout = CheckoutPage(self.driver)
-        self.custom = CustomizePage(self.driver)
-
-        self.lp.login('jason.pacitti@yeti.com', 'tester123')
+@pytest.mark.usefixtures("setup")
+class TestCustomLogoOrder():
 
     def tests_custom_logo_order(self):
-        product_url = 'https://Storefront:Yeti2017@staging-na-yeti.demandware.net/s/Yeti_US/en_US/drinkware/rambler-20-oz-tumbler/YRAM20.html'
-        self.driver.get(product_url)
+        pdp = ProductPage(self.driver)
+        checkout = CheckoutPage(self.driver)
+        custom = CustomizePage(self.driver)
+        pdp.pdpSwatches('Black')
 
-        self.pdp.pdpSwatches('Black')
+        custom.pdpClickCustomButton()
+        custom.customModal()
+        custom.selectCustomLogo()
+        custom.clickApproval()
+        custom.clickAddToCart()
 
-        self.custom.pdpClickCustomButton()
-        self.custom.customModal()
-        self.custom.selectCustomLogo()
-        self.custom.clickApproval()
-        self.custom.clickAddToCart()
+        pdp.clickMiniCart()
 
-        self.pdp.clickMiniCart()
-
-        self.checkout.shippingBtn()
-        self.checkout.accountPayment('111')
+        checkout.shippingBtn()
+        checkout.accountPayment('111')
 
         order_number = self.driver.find_element(By.XPATH, '//p[@class="order-number"]//a').text
         print(order_number)
-
-    def tearDown(self):
-        self.driver.quit()
