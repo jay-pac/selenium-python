d58e3c593d31581fc40caf5070290e3f10374920
Jason Pacitti <jpacitti@gmail.com>
2020-02-26 22:14:55 -0600

    adding new conftest for login tests

---
+++ sfcc/tests/login/conftest.py
@@ -0,0 +1,67 @@
+from selenium import webdriver
+import pytest
+
+driver = None
+
+
+def pytest_addoption(parser):
+    parser.addoption(
+        "--browser_name", action="store", default="chrome"
+    )
+    parser.addoption(
+        "--env_name", action="store", default="dev-us"
+    )
+
+
+@pytest.fixture(scope="class")
+def setup(request):
+    global driver
+    browser_name = request.config.getoption("browser_name")
+    if browser_name == "chrome":
+        driver = webdriver.Chrome()
+    elif browser_name == "firefox":
+        driver = webdriver.Firefox()
+    elif browser_name == "IE":
+        print("IE driver is not an option")
+
+    env_name = request.config.getoption("env_name")
+    if env_name == "dev-us":
+        driver.get('https://Storefront:Yeti2017@development-na-yeti.demandware.net/s/Yeti_US/en_US/login')
+        cookie = {
+            'domain': 'development-na-yeti.demandware.net',
+            'httpOnly': False,
+            'name': 'consent-accepted',
+            'path': '/',
+            'secure': False,
+            'value': 'true'}
+        driver.add_cookie(cookie)
+
+    elif env_name == 'stg-us':
+        driver.get('https://Storefront:Yeti2017@staging-na-yeti.demandware.net/s/Yeti_US/en_US/login')
+        cookie = {
+            'domain': 'staging-na-yeti.demandware.net',
+            'httpOnly': False,
+            'name': 'consent-accepted',
+            'path': '/',
+            'secure': False,
+            'value': 'true'}
+        driver.add_cookie(cookie)
+
+    elif env_name == 'prod-us':
+        driver.get('https://www.yeti.com/en_US/login')
+        cookie = {
+            'domain': 'www.yeti.com',
+            'httpOnly': False,
+            'name': 'consent-accepted',
+            'path': '/',
+            'secure': False,
+            'value': 'true'}
+        driver.add_cookie(cookie)
+
+    driver.implicitly_wait(10)
+    driver.refresh()
+    driver.maximize_window()
+
+    request.cls.driver = driver
+    yield
+    driver.close()
