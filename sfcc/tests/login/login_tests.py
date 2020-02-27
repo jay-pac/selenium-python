2b81ddada61abb5395317e583dd2901c91f7a230
Jason Pacitti <jpacitti@gmail.com>
2020-02-26 22:18:54 -0600

    removed setup and added it the conftest and added fixtures

--- sfcc/tests/login/login_tests.py
+++ sfcc/tests/login/login_tests.py
@@ -1,33 +1,12 @@
-from selenium import webdriver
 from sfcc.pages.login.login_page import LoginPage
-import unittest
+import pytest
 
 
-class LoginTests(unittest.TestCase):
-
-    def setUp(self):
-        base_url = 'https://Storefront:Yeti2017@staging-na-yeti.demandware.net/s/Yeti_US/en_US/login'
-        self.driver = webdriver.Chrome()
-        self.driver.maximize_window()
-        self.driver.implicitly_wait(10)
-        self.driver.get(base_url)
-
-        cookie = {
-            'domain': 'staging-na-yeti.demandware.net',
-            'httpOnly': False,
-            'name': 'consent-accepted',
-            'path': '/',
-            'secure': False,
-            'value': 'true'}
-        self.driver.add_cookie(cookie)
-        self.driver.refresh()
-
-        self.lp = LoginPage(self.driver)
+@pytest.mark.usefixtures("setup")
+class TestLogin():
 
     def tests_login(self):
-        self.lp.login('generalOne@user.com', 'Generalone19!')
+        lp = LoginPage(self.driver)
+        lp.login('jason.pacitti@yeti.com', 'T3ster#!')
 
         assert 'My Account Home' in self.driver.title
-
-    def tearDown(self):
-        self.driver.quit()
