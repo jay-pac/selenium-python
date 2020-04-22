ee9e5380fa1ca0aeff2631d4a3ff57b01cfe12ab
Jason Pacitti <jpacitti@gmail.com>
2020-04-22 14:36:36 -0500

    removed unused modules

--- sfcc/tests/customizer/custom_design_order_tests.py
+++ sfcc/tests/customizer/custom_design_order_tests.py
@@ -1,6 +1,4 @@
-from selenium import webdriver
 from selenium.webdriver.common.by import By
-from sfcc.pages.login.login_page import LoginPage
 from sfcc.pages.customize_page import CustomizePage
 from sfcc.pages.product_page import ProductPage
 from sfcc.pages.checkout_page import CheckoutPage
