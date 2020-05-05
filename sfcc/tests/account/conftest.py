6f279a285c58d863d917217c1baaef4f7b814142
Jason Pacitti <jpacitti@gmail.com>
2020-05-05 16:56:13 -0500

    changed scope from class to function

--- sfcc/tests/account/conftest.py
+++ sfcc/tests/account/conftest.py
@@ -13,7 +13,7 @@ def pytest_addoption(parser):
     )
 
 
-@pytest.fixture(scope="class")
+@pytest.fixture(scope="function")
 def setup(request):
     global driver
     browser_name = request.config.getoption("browser_name")
