79511216d04676f1012cdda6fa2b5b2bd9c0ad11
Jason Pacitti <jpacitti@gmail.com>
2020-05-12 20:08:24 -0500

    Added verify address method

--- sfcc/tests/customizer/custom_design_order_tests.py
+++ sfcc/tests/customizer/custom_design_order_tests.py
@@ -24,6 +24,7 @@ class TestCustomDesignOrder():
         pdp.clickMiniCart()
 
         checkout.shippingBtn()
+        checkout.clickVerifyAddress()
         checkout.accountPayment('111')
 
         order_number = self.driver.find_element(By.XPATH, '//p[@class="order-number"]//a').text
